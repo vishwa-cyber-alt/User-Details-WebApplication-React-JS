47fb48eb7a020c595db270a197539497bd820cd2
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-08-13 18:31:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,48 @@
+import './styles.css';
+
+function App() {
+return (
+  
+<div className="App">
+	<table>
+		<tr>
+			<th>Name</th>
+			<th>Age</th>
+			<th>Gender</th>
+		</tr>
+		<tr>
+			<td>Vishwa V</td>
+			<td>21</td>
+			<td>Male</td>
+		</tr>
+		<tr>
+			<td>Megha</td>
+			<td>19</td>
+			<td>Female</td>
+		</tr>
+		<tr>
+			<td>Ranveer</td>
+			<td>40</td>
+			<td>Male</td>
+		</tr>
+    <tr>
+      <td>Ajith</td>
+      <td>53</td>
+      <td>Male</td></tr>
+
+      <tr>
+      <td>salman</td>
+      <td>33</td>
+      <td>Male</td></tr>
+      <tr>
+      <td>johncena</td>
+      <td>77</td>
+      <td>Male</td></tr>
+
+	</table>
+</div>
+
+);
+}
+
+export default App;
